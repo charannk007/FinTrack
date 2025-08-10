b4d8d327b33e16b7bdc6795e197a6dcb847c08d2
Charan NK <60515249+charannk007@users.noreply.github.com>
2025-08-10 09:54:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,38 +1,36 @@
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('❌ Database connection failed:', err.stack);
-    process.exit(1);
-  }
-  console.log('✅ Connected to MySQL Database');
-});
-
-module.exports = connection;
-
-
 // const mysql = require('mysql2');
 
 // const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root123',
-//   database: 'fintrack_db'
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
 // });
 
 // connection.connect((err) => {
 //   if (err) {
 //     console.error('❌ Database connection failed:', err.stack);
-//     return;
+//     process.exit(1);
 //   }
 //   console.log('✅ Connected to MySQL Database');
 // });
 
 // module.exports = connection;
+
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'fintrack.czccaimeyk2a.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'admin12345678',
+  database: 'fintrack_db'
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err.stack);
+    return;
+  }
+  console.log('✅ Connected to MySQL Database');
+});
+
+module.exports = connection;
